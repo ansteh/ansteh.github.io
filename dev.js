4153684b895b1b0af4b542403e351ef110a761c5
ansteh <stehle.andre@googlemail.com>
2016-07-07 00:20:49 +0200

    roi polymer

--- dev.js
+++ dev.js
@@ -3,6 +3,7 @@
 const express        = require('express');
 const app            = express();
 const path           = require('path');
+const fs             = require('fs');
 
 app.use('/client', express.static(path.join(__dirname, '/client')));
 app.use('/roi', express.static(path.join(__dirname, '/roi')));
@@ -10,6 +11,18 @@ app.use('/bower_components', express.static(path.join(__dirname, '/bower_compone
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+
+['FB', 'GOOG'].forEach((name) => {
+  app.get(`/resources/${name}.json`, function(req, res) {
+    fs.readFile(`resources/${name.toLowerCase()}.json`, 'utf8', (err, content) => {
+      if(err) {
+        res.status(404);
+      } else {
+        res.json(JSON.parse(content));
+      }
+    })
+  });
 });
 
 const server = require('http').Server(app);
