3ccf42093ea6f1ec0378d3ae187c893316999e86
ansteh <stehle.andre@googlemail.com>
2016-07-06 22:02:05 +0200

    bower_components

--- dev.js
+++ dev.js
@@ -6,6 +6,7 @@ const path           = require('path');
 
 app.use('/client', express.static(path.join(__dirname, '/client')));
 app.use('/roi', express.static(path.join(__dirname, '/roi')));
+app.use('/bower_components', express.static(path.join(__dirname, '/bower_components')));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
